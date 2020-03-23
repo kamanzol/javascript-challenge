43a64f7a7cdc54e56c9e5563fbbedb32d215e010
kamanzol <katie.manzoline@gmail.com>
2020-03-23 17:04:37 -0500

    almost there

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,5 +22,17 @@ var tbody = d3.select("tbody");
 // for events and search through the date/time column to find rows that match user input.
 
 
+var tableDatafilter = data;
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filteredData = tableDatafilter.filter(tableDatafilter => tableDatafilter.datetime === inputValue);
+
+  console.log(filteredData); 
+
+});
 
 
