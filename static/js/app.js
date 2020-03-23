52e0f6b3f2e091b85a97eb53877003617fcebe7e
kamanzol <katie.manzoline@gmail.com>
2020-03-23 15:40:21 -0500

    through inserting data to table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+// Using the UFO dataset provided in the form of an array of JavaScript objects, 
+// write code that appends a table to your web page and then adds new rows of data 
+// for each UFO sighting.
+
+var tbody = d3.select("tbody");
+
+ tableData.forEach(function(UFOSighting) {
+  var row = tbody.append("tr");
+   Object.entries(UFOSighting).forEach(function([key, value]) {
+   var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Use a date form in your HTML document and write JavaScript code that will listen 
+// for events and search through the date/time column to find rows that match user input.
+
+
+
+
