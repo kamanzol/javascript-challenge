3b0950f11cf796b424b59a8aaefff5ab51ba70b8
kamanzol <katie.manzoline@gmail.com>
2020-03-23 22:29:17 -0500

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,6 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-
 // Using the UFO dataset provided in the form of an array of JavaScript objects, 
 // write code that appends a table to your web page and then adds new rows of data 
 // for each UFO sighting.
@@ -21,18 +20,23 @@ var tbody = d3.select("tbody");
 // Use a date form in your HTML document and write JavaScript code that will listen 
 // for events and search through the date/time column to find rows that match user input.
 
+// renderTable(tableData)
+// var EnterDate = {}
 
-var tableDatafilter = data;
 var button = d3.select("#filter-btn");
-
 button.on("click", function() {
+  // EnterDate = {};
 
   var inputElement = d3.select("#datetime");
   var inputValue = inputElement.property("value");
-  var filteredData = tableDatafilter.filter(tableDatafilter => tableDatafilter.datetime === inputValue);
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
   console.log(filteredData); 
 
 });
 
+
+
+// remove all table data 
+// then try to do a new function with filtered options
 
